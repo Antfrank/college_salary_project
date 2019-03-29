e32a670b592123a5c52b5e1d64e0b20602b0c644
Antfrank <A.darnell.f18@gmail.com>
2019-03-29 19:33:20 +0000

    first commit

--- college_analysis.R
+++ college_analysis.R
@@ -4,7 +4,11 @@ library(janitor)
 library(tidyverse)
 library(readr)
 degrees_that_pay_back <- read_csv("code/raw_code/degrees-that-pay-back.csv") %>%
-  clean_names()
+  clean_names() %>% 
+  mutate(starting_median_salary = as.numeric(gsub('\\$|,', '', starting_median_salary))
+) %>% 
+  mutate(mid_career_median_salary = as.numeric(gsub('\\$|,', '', mid_career_median_salary))
+  )
 
 salaries_by_college_type <- read_csv("code/raw_code/salaries-by-college-type.csv") %>%
   clean_names()
@@ -13,13 +17,7 @@ salaries_by_region <- read_csv("code/raw_code/salaries-by-region.csv") %>%
   clean_names()
 
 
-p1 <- ggplot(data= degrees_that_pay_back, aes(x= reorder(undergraduate_major, -starting_median_salary), y=starting_median_salary)) +
-  geom_bar(stat="identity", fill = "steelblue") +
-  coord_flip()
 
-p2 = ggplot(data= degrees_that_pay_back, aes(x=reorder(undergraduate_major, -mid_career_median_salary), y=mid_career_median_salary)) +
-  geom_bar(stat="identity", fill = "red") +
-  coord_flip()
 
 install.packages("gridExtra")
 library(gridExtra)
@@ -27,11 +25,26 @@ grid.arrange(p1,p2, ncol = 2)
 
 library(scales)
 
+class(degrees_that_pay_back$starting_median_salary)
+
+install.packages("stingr")
 
 
- 
 
+p1 <- ggplot(data= degrees_that_pay_back, aes(x= fct_reorder(undergraduate_major,starting_median_salary), y=starting_median_salary)) +
+  geom_bar(stat="identity", fill = "steelblue") +
+  coord_flip()
+p1
 
+p2 = ggplot(data= degrees_that_pay_back, aes(x= fct_reorder(undergraduate_major,mid_career_median_salary), y=mid_career_median_salary)) +
+  geom_bar(stat="identity", fill = "red") +
+  coord_flip()
+
+p2
+
+install.packages("gridExtra")
+library(gridExtra)
+grid.arrange(p1,p2, ncol = 2)
 
 
 
